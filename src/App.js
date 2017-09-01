f30f86d57032e94297f1849f8cd521994cd4e272
manoelango <31500057+manoelango@users.noreply.github.com>
2017-09-01 07:18:42 +0530

    commented the shelf set line to prevent typeerror

--- src/App.js
+++ src/App.js
@@ -20,21 +20,11 @@ class BooksApp extends Component {
       books: bookData
     })
   }
-
-//   moveBook(book, toShelf) {
-//    BooksAPI.getAll().then((data) => {
-//       BooksAPI.update(book, toShelf).then(
-//         this.setState((state) => ({
-//           books: state.books.filter((b) => b.id !== book.id).concat([book])
-//         }))
-//       )
-//     })
-//   }
   
   moveBook(book, toShelf) {
     BooksAPI.update(book, toShelf).then(response => {
       // make sure you set the shelf!
-      book.shelf = toShelf  
+      //book.shelf = toShelf  
       this.setState((state) => ({
         books: state.books.filter(b => b.id !== book.id).concat([book])
       }))
